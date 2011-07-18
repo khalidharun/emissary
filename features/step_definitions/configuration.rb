8dca8baaf03b243728bec8df10b382ec83f78681
Chris DiMartino <vwchris@gmail.com>
2011-07-18 18:32:51 -0400

    BDD Updates
    - Added features and spec dirs

---
+++ features/step_definitions/configuration.rb
@@ -0,0 +1,32 @@
+Given /^I have not created a configuration file$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^Emissary starts$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it should raise an error$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^I have created a configuration file$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^it is not a well formed configuration file$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^it is a well formed configuration file$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^is should not raise an error$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it should daemonize$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
